530e8428e5f8bc4fef04a71cabe9f6f313492668
apps4zap <52222876+apps4zap@users.noreply.github.com>
2019-07-01 12:55:37 -0500

    Merge schema

--- studio/schemas/schema.js
+++ studio/schemas/schema.js
@@ -4,6 +4,26 @@ import fulfillment from './fulfillment';
 import speech from './ssml-editor/speech';
 import { alias, emphasis, prosody, sayAs } from './ssml-editor/annotations';
 
+// document schemas
+import author from './documents/author'
+import category from './documents/category'
+import post from './documents/post'
+import siteSettings from './documents/siteSettings'
+
+// Object types
+import bodyPortableText from './objects/bodyPortableText'
+import bioPortableText from './objects/bioPortableText'
+import excerptPortableText from './objects/excerptPortableText'
+import mainImage from './objects/mainImage'
+import authorReference from './objects/authorReference'
+
+// Object types
+import bodyPortableText from './objects/bodyPortableText'
+import bioPortableText from './objects/bioPortableText'
+import excerptPortableText from './objects/excerptPortableText'
+import mainImage from './objects/mainImage'
+import authorReference from './objects/authorReference'
+
 export default createSchema({
   name: 'default',
   types: schemaTypes.concat([
@@ -13,5 +33,14 @@ export default createSchema({
     sayAs,
     fulfillment,
     speech,
+    siteSettings,
+    post,
+    category,
+    author,
+    mainImage,
+    authorReference,
+    bodyPortableText,
+    bioPortableText,
+    excerptPortableText
   ]),
 });
