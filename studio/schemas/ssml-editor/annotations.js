bffcdab5340a0a46651bce9aa671ac3f0fc934fc
apps4zap <52222876+apps4zap@users.noreply.github.com>
2019-06-30 19:47:18 -0500

    Paste

--- studio/schemas/ssml-editor/annotations.js
+++ studio/schemas/ssml-editor/annotations.js
@@ -1,1 +1,6 @@
 
+// annotations.js
+export {default as alias} from './alias'
+export {default as emphasis} from './emphasis'
+export {default as prosody} from './prosody'
+export {default as sayAs} from './sayAs'
