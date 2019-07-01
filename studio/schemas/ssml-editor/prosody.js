ee2030e2bd4bce24d897cc5425a0baeb97fd1847
apps4zap <52222876+apps4zap@users.noreply.github.com>
2019-07-01 12:43:10 -0500

    Full react

--- studio/schemas/ssml-editor/prosody.js
+++ studio/schemas/ssml-editor/prosody.js
@@ -1,25 +1,65 @@
 import React from 'react'
 export default {
-  name: 'emphasis',
+  name: 'prosody',
   type: 'object',
-  title: 'Emphasis',
-  description: 'The strength of the emphasis put on the contained text',
+  title: 'Prosody',
+  description: 'Control of the pitch, speaking rate, and volume',
   fields: [
     {
-      name: 'level',
+      name: 'pitch',
       type: 'string',
+      title: 'Pitch',
+      description: 'The baseline pitch for the contained text',
       options: {
         list: [
-          { value: 'strong', title: 'Strong' },
-          { value: 'moderate', title: 'Moderate' },
-          { value: 'none', title: 'None' },
-          { value: 'reduced', title: 'Reduced' },
+          { value: 'x-low', title: 'Extra low' },
+          { value: 'low', title: 'Low' },
+          { value: 'medium', title: 'Medium' },
+          { value: 'high', title: 'High' },
+          { value: 'x-high', title: 'Extra high' },
+          { value: 'default', title: 'Default' },
+        ],
+      },
+    },
+    {
+      name: 'rate',
+      type: 'string',
+      title: 'Rate',
+      description: 'A change in the speaking rate for the contained text',
+      options: {
+        list: [
+          { value: 'x-slow', title: 'Extra slow' },
+          { value: 'slow', title: 'Slow' },
+          { value: 'medium', title: 'Medium' },
+          { value: 'fast', title: 'Fast' },
+          { value: 'x-fast', title: 'Extra fast' },
+          { value: 'default', title: 'Default' },
+        ],
+      },
+    },
+    {
+      name: 'volume',
+      type: 'string',
+      title: 'Volume',
+      description: 'The volume for the contained text.',
+      options: {
+        list: [
+          { value: 'silent', title: 'Silent' },
+          { value: 'x-soft', title: 'Extra soft' },
+          { value: 'medium', title: 'Medium' },
+          { value: 'loud', title: 'Loud' },
+          { value: 'x-loud', title: 'Extra loud' },
+          { value: 'default', title: 'Default' },
         ],
       },
     },
   ],
   blockEditor: {
-    icon: () => '🗯',
-    render: ({ children }) => <span>{children} 🗯</span>,
+    icon: () => '🔊',
+    render: ({ children, volume }) => (
+      <span>
+        {children} {['x-loud', 'loud'].includes(volume) ? '🔊' : '🔈'}
+      </span>
+    ),
   },
 };
