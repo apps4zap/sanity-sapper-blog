a5a5c56180206758040400292b35a5b9fccb0af4
apps4zap <52222876+apps4zap@users.noreply.github.com>
2019-06-30 20:55:36 -0500

    Paste

--- studio/schemas/ssml-editor/SSMLeditor.js
+++ studio/schemas/ssml-editor/SSMLeditor.js
@@ -1,1 +1,13 @@
 
+import React, { Fragment } from 'react';
+import { BlockEditor } from 'part:@sanity/form-builder';
+import PreviewButton from './PreviewButton';
+
+export default function SSMLeditor(props) {
+  return (
+    <Fragment>
+      <BlockEditor {...props} />
+      <PreviewButton blocks={props.value} />
+    </Fragment>
+  );
+}
