6eef163020da67db8bf48f28246cc98fe64012f0
apps4zap <52222876+apps4zap@users.noreply.github.com>
2019-06-30 19:48:49 -0500

    Annotations

--- studio/schemas/ssml-editor/speech.js
+++ studio/schemas/ssml-editor/speech.js
@@ -1,10 +1,22 @@
-
 // speech.js
 export default {
   name: 'speech',
   type: 'array',
   title: 'SSML Editor',
   of: [
-    { type: 'block' }
+    {
+      type: 'block',
+      styles: [],
+      lists: [],
+      marks: {
+        decorators: [],
+        annotations: [
+          {type: 'alias'},
+          {type: 'emphasis'},
+          {type: 'prosody'},
+          {type: 'sayAs'}
+        ]
+      }
+    }
   ]
 }
