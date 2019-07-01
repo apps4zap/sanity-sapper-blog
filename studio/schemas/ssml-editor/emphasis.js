bb8ea34a8a71f2d02b7bde2a8f1c0f3495af70d5
apps4zap <52222876+apps4zap@users.noreply.github.com>
2019-06-30 19:43:00 -0500

    Paste

--- studio/schemas/ssml-editor/emphasis.js
+++ studio/schemas/ssml-editor/emphasis.js
@@ -1,1 +1,23 @@
 
+// emphasis.js
+export default {
+  name: 'emphasis',
+  type: 'object',
+  title: 'Emphasis',
+  description:
+    'The strength of the emphasis put on the contained text',
+  fields: [
+    {
+      name: 'level',
+      type: 'string',
+      options: {
+        list: [
+          { value: 'strong', title: 'Strong' },
+          { value: 'moderate', title: 'Moderate' },
+          { value: 'none', title: 'None' },
+          { value: 'reduced', title: 'Reduced' }
+        ]
+      }
+    }
+  ]
+}
