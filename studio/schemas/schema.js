6c74925b14c699f26fef225c2cc965c0db8c35c8
apps4zap <52222876+apps4zap@users.noreply.github.com>
2019-07-02 13:43:16 -0500

    blocksToSSML

--- studio/schemas/schema.js
+++ studio/schemas/schema.js
@@ -2,7 +2,7 @@ import createSchema from 'part:@sanity/base/schema-creator';
 import schemaTypes from 'all:part:@sanity/base/schema-type';
 import fulfillment from './fulfillment';
 import speech from './ssml-editor/speech';
-import { alias, emphasis, prosody, sayAs } from './ssml-editor/annotations';
+import { alias, emphasis, prosody, blocksToSSML, sayAs } from './ssml-editor/annotations';
 
 // document schemas
 import author from './documents/author'
@@ -34,6 +34,7 @@ export default createSchema({
     authorReference,
     bodyPortableText,
     bioPortableText,
-    excerptPortableText
+    excerptPortableText,
+    blocksToSSML
   ]),
 });
